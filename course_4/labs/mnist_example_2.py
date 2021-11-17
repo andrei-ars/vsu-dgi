7a7d7e80b6c720be21e9fb8fac90be148c056c71
Andrei <z@sp.sp>
2021-11-17 13:48:38 +0300

    exam q

--- course_4/labs/mnist_example_2.py
+++ course_4/labs/mnist_example_2.py
@@ -66,7 +66,7 @@ class NeuralNetworkModel(nn.Module):
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.drop2 = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
+        self.fc1 = nn.Linear(3  20, 50)
         self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
